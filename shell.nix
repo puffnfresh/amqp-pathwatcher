4a2da8f6a5614abbbba8f39f19ad6c03ae8cd54e
Brian McKenna <brian@simpleenergy.com>
2014-11-06 11:37:02 -0700

    configurator + optparse-applicative are in nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,6 +1,1 @@
-((import <nixpkgs> {}).haskellPackages.override {
-  extension = self: super: {
-    configurator = self.callPackage ./configurator.nix {};
-    optparseApplicative = self.callPackage ./optparseApplicative.nix {};
-  };
-}).callPackage ./default.nix {}
+(import <nixpkgs> {}).haskellPackages.callPackage ./default.nix {}
